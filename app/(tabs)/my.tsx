6281366d647815cee7ff8ddd575c46ddc708741c
hec9527 <hec9527@foxmail.com>
2025-07-24 16:10:41 +0800

    小改动

---
+++ app/(tabs)/my.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Comp: React.FC = () => {
+  return (
+    <View>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi molestiae non esse consequatur, autem qui
+        corrupti error voluptas. Praesentium iste, beatae perferendis alias vel repellendus excepturi ipsum odit dolores
+        perspiciatis?
+      </Text>
+    </View>
+  );
+};
+
+Comp.displayName = 'Comp';
+export default Comp;
